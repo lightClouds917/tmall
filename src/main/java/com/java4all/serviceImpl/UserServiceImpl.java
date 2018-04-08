d9bec437bde08274d227cf55605305a026565e8d
wangzhongxiang <qwerty>
2018-04-08 18:25:50 +0800

    fix jdbcTemplate 列表查询

--- src/main/java/com/java4all/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/java4all/serviceImpl/UserServiceImpl.java
@@ -5,12 +5,12 @@ import com.java4all.entity.User;
 import com.java4all.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Author: momo
@@ -34,7 +34,7 @@ public class UserServiceImpl implements UserService {
     /**       JdbcTemplate      */
     @Override
     public Integer getCount() {
-        String sql = "SELECT COUNT (*) FROM user";
+        String sql = "SELECT COUNT(*) FROM user";
         Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
         return count;
     }
@@ -53,18 +53,32 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getUserByProvinceAndCity(String province, String city) {
+        //错误写法，如果打印出此sql，会发现?还是?，并没有被参数替换掉
         String sql = "select * from user where 1= 1 and province = ? and city = ?";
 
-        List<User> list = jdbcTemplate.query(sql, new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet resultSet, int i) throws SQLException {
-                User user = new User();
-                user.setId(resultSet.findColumn("id"));
-                user.setUserName(resultSet.getNString("user_name"));
-                return user;
-            }
-        }, province, city);
+        String sql2 = "select * from user where 1= 1 and province ='"+ province +"' and city ='"+city + "'";
 
-        return list;
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql2);
+
+        List<User> listRe = new ArrayList<>();
+        for(int i = 0 , length = list.size();i < length; i ++){
+            Map<String, Object> map = list.get(i);
+            User user = new User();
+            //这个map转为实体的方式，可以抽取出来，其他方法可复用
+            user.setId((int)map.get("id"));
+            user.setUserName((String)map.get("user_name"));
+            user.setRealName((String)map.get("real_name"));
+            user.setPassword((String)map.get("password"));
+            user.setCountry((String)map.get("country"));
+            user.setProvince((String)map.get("province"));
+            user.setCity((String)map.get("city"));
+            user.setPhone((String)map.get("phone"));
+            user.setCreateTime((Date) map.get("create_time"));
+            user.setDevLanguage((String)map.get("dev_language"));
+            user.setSchoolName((String)map.get("school_name"));
+            user.setSchoolMajor((String)map.get("school_major"));
+            listRe.add(user);
+        }
+        return listRe;
     }
 }
