b8f93079d6795ababdf72c11bdfa486798effaf1
汪忠祥 <1186355422@qq.com>
2019-10-10 14:43:38 +0800

    remove

---
+++ src/main/java/com/java4all/kafka/Consumer.java
@@ -0,0 +1,9 @@
+package com.java4all.kafka;
+
+/**
+ * @author wangzhongxiang
+ * @date 2019年05月26日 11:02:37
+ */
+public class Consumer {
+
+}
---
+++ src/main/java/com/java4all/kafka/Producer.java
@@ -0,0 +1,9 @@
+package com.java4all.kafka;
+
+/**
+ * @author wangzhongxiang
+ * @date 2019年05月26日 11:01:39
+ */
+public class Producer {
+
+}
